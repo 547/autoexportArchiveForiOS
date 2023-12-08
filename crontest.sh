436ef02cece669ed0fe7eabeb8b055aca7e7cfb0
wangqi <timer_sevenwang@163.com>
2023-12-08 11:36:07 +0800

    Update crontest.sh

--- crontest.sh
+++ crontest.sh
@@ -2,5 +2,3 @@
 dateString=`date +"%Y/%m/%d %H:%M:%S"`
 log="执行时间 $dateString"
 echo $log >> /Users/momo/Desktop/crontab_test_log/logs.txt
-Fri Dec  8 11:13:00 CST 2023
-Fri Dec  8 11:14:00 CST 2023
