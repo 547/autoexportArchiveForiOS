8d08ab6104355e2b49ee43baca2e0f133032f9cd
wangqi <timer_sevenwang@163.com>
2023-12-08 23:17:07 +0800

    Update autoexport_archive.sh

--- autoexport_archive.sh
+++ autoexport_archive.sh
@@ -122,7 +122,7 @@ function checkoutBranch()
 function getGitLogs()
 {   
     local branch=`git branch --show-current`
-    local logs=$(git log $branch -5 --color --graph --pretty=format:'-%C(cyan)%s %C(magenta)(%cr)')
+    local logs=$(git log $branch -5 --color --pretty=format:'-%C(cyan)%s %C(magenta)(%cr)')
     local result="\n$1\n当前分支：$branch\n$logs\n"
     echo $result
     if test $1 == "flutter" 
@@ -298,7 +298,4 @@ function uploadPgyer()
 preparation
 releaseFlutterProject
 releaseiOSProject
-if test $ios_method = "ad-hoc"
-then
-    uploadPgyer
-fi
+uploadPgyer
