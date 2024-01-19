0028edb9f2cb11c39bda40c3defddba96ca96fb0
lyl <549881819@qq.com>
2024-01-19 13:39:47 +0800

    修改为 122灰度版本

--- autoexport_archive.sh
+++ autoexport_archive.sh
@@ -6,13 +6,13 @@ set -e
 # 不是用crontab可以注释掉
 . /Users/momo/.zshrc;
 # 环境 1:开发 2:测试 3:灰度 4:生产 5:预生产
-environment=2
+environment=3
 
 # flutter 项目打包需要使用的分支
-flutter_branch="dev_stock"
+flutter_branch="release_24_01_22"
 
 # iOS 项目打包需要使用的分支
-ios_branch="developer"
+ios_branch="release_1.0.65"
 
 # 打包方式 app-store、ad-hoc
 ios_method="ad-hoc"
